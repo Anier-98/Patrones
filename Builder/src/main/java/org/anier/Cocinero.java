7740af7ff701c499f4c45fc6d8aa5ebfed96ab9a
Anier-98 <anierf3@gmail.com>
2021-10-22 11:02:26 -0600

    ejercicio realizado en horario de clases

---
+++ Builder/src/main/java/org/anier/Cocinero.java
@@ -0,0 +1,21 @@
+package org.anier;
+
+public class Cocinero {
+    private PizzaBuilder pizzaBuilder;
+
+    public void setPizzaBuilder(PizzaBuilder pb){
+        pizzaBuilder=pb;
+    }
+
+    public Pizza getPizza(){
+        return pizzaBuilder.getPizza();
+    }
+
+    public void crearPizza(){
+        pizzaBuilder.crearNuevaPizza();
+        pizzaBuilder.ponerNombre();
+        pizzaBuilder.crearMasa();
+        pizzaBuilder.crearSalsa();
+        pizzaBuilder.crearIngrediente();
+    }
+}
